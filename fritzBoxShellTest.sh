6b8c7abc84d74efe304a88a78561551e8e7280a1
bear-net <github@bear-net.de>
2020-11-11 20:32:56 +0100

    Improved test script output

--- fritzBoxShellTest.sh
+++ fritzBoxShellTest.sh
@@ -13,19 +13,61 @@ dir=$(dirname "$0")
 DIRECTORY=$(cd "$dir" && pwd)
 source "$DIRECTORY/fritzBoxShellConfig.sh"
 
+# Set Colors
+reset=$(tput sgr0)
+cyan=$(tput setaf 6)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+tan=$(tput setaf 3)
+blue=$(tput setaf 6)
+
+e_success() {
+	slen="$1"
+	alen="$2"
+	shift; shift
+	printf "${green}\\xE2\\x9C\\x94 %-${slen}s %-${alen}s %s${reset}\\n" "$@"
+}
+
+e_error() {
+	len="$1"
+	alen="$2"
+	shift; shift
+  printf "${red}\\xE2\\x9C\\x96 %-${len}s %-${alen}s %s${reset}\\n" "$@"
+}
+
+
 ## declare an array variable
 declare -a services=("WLAN_2G"   "WLAN_2G" "WLAN_5G"    "WLAN_2G" "WLAN"  "LAN"   "DSL"   "WAN"   "LINK"  "IGDWAN" "IGDDSL" "IGDIP" "TAM")
 declare -a actions=("STATISTICS" "STATE"   "STATISTICS" "STATE"   "STATE" "STATE" "STATE" "STATE" "STATE" "STATE"  "STATE"  "STATE" "0 GetInfo")
 declare -a minwords=(3           5         3            5         9       1       1       1       1       1        1        1       5      )
 
+## loop through array to count word lengths
+counter=0
+maxservicelen=0
+maxactionlen=0
+for i in "${services[@]}"
+do
+	servicelen=${#i}
+	actionlen=${#actions[$counter]}
+	if [[ "$servicelen" -ge "$maxservicelen" ]]; then
+		maxservicelen="$servicelen"
+	fi
+	if [[ "$actionlen" -ge "$maxactionlen" ]]; then
+		maxactionlen="$actionlen"
+	fi
+	counter=$((counter+1))
+done
+
 ## now loop through the above array
 counter=0
 for i in "${services[@]}"
 do
-	echo -n "$i" "${actions[$counter]}"
 	words=$(/bin/bash "$DIRECTORY/fritzBoxShell.sh" "$i" "${actions[$counter]}"|wc -w)
-	#echo -n $words
-	[[ "$words" -ge ${minwords[$counter]} ]] && echo -e "\tis working!" || echo -e "\tis not working!"
+	if [[ "$words" -ge ${minwords[$counter]} ]]; then
+		e_success "$maxservicelen" "$maxactionlen" "$i" "${actions[$counter]}" "is working"
+	else
+		e_error "$maxservicelen" "$maxactionlen" "$i" "${actions[$counter]}" "is not working"
+	fi
 	counter=$((counter+1))
 done
 /bin/bash "$DIRECTORY/fritzBoxShell.sh" DEVICEINFO 3 | grep NewModelName
